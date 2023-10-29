d146cdeaaa96e10057ef7f47c6896a0092992cf4
Calvin Che <calvinkpche@gmail.com>
2023-10-29 13:42:17 -0700

    Created basic skeleton Template for navbar using Jquery

---
+++ scripts/skeleton.js
@@ -0,0 +1,5 @@
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('./components/navbar.html'));
+    console.log($('#headerPlaceholder').load('./components/header.html'));
+}
+loadSkeleton();
