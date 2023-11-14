a723cc513cfc5f699999c7f53979578aa9f2e2e6
Tromulus <thomassmith@live.ca>
2023-11-13 21:51:35 -0800

    Added onclick to activity buttons that disables button and gives user points.

--- scripts/activities.js
+++ scripts/activities.js
@@ -1,3 +1,18 @@
+const testUserID = "cNsDxf94TxsP8HOsH2IK";
+var currentUser;
+firebase.auth().onAuthStateChanged(user => {
+    // Check if user is signed in:
+    if (user) {
+        //go to the correct user document by referencing to the user uid
+        currentUser = db.collection("users").doc(user.uid)
+        //get the document for current user.
+        currentUser.get()
+        console.log(user.uid);
+    } else {
+        console.log("error");
+    }
+})
+console.log(currentUser);
 //------------------------------------------------------------------------------
 // Input parameter is a string representing the collection we are reading from
 //------------------------------------------------------------------------------
@@ -6,7 +21,7 @@ function displayCardsDynamically(collection) {
 
     db.collection(collection).get()   //the collection
         .then(allActivity=> {
-            //var i = 1;  //Optional: if you want to have a unique ID for each hike
+            var i = 1;  //Optional: if you want to have a unique ID for each hike
             allActivity.forEach(doc => { //iterate thru each doc
                 var title = doc.data().name;       // get value of the "name" key
                 var description = doc.data().description;  // get value of the "details" key
@@ -19,22 +34,55 @@ function displayCardsDynamically(collection) {
 
                 //update title and text and image
                 newcard.querySelector('.card-title').innerHTML = title;
-                newcard.querySelector('.card-length').innerHTML = score;
+                newcard.querySelector('.card-points').innerHTML = score + " points.";
                 newcard.querySelector('.card-text').innerHTML = description;
                 // newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
                 // newcard.querySelector('a').href = ".html?docID="+docID;
 
                 //Optional: give unique ids to all elements for future use
-                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
                 // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
                 // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
 
                 //attach to gallery, Example: "hikes-go-here"
                 document.getElementById(collection + "-go-here").appendChild(newcard);
 
-                //i++;   //Optional: iterate variable to serve as unique ID
+                i++;   //Optional: iterate variable to serve as unique ID
             })
         })
 }
 
 displayCardsDynamically("activity");  //input param is the name of the collection
+
+function completeActivity(button) {
+    var userPoints;
+    var newPoints;
+    button.className="btn btn-primary card-href disabled"
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid)
+            //get the document for current user.
+            currentUser.get().then(userDoc => {
+                var userLevel = userDoc.date().level;
+                userPoints = userDoc.data().points;
+                newPoints = userPoints + 5;
+                currentUser.update({
+                    points: newPoints
+                })
+            })
+        }
+    })   
+    console.log(currentUser);
+}
+
+function calculateUserLevel(exp, currentLevel) {
+     var expRequire = 100 * (currentLevel ** 1.1)
+     if (exp >= expRequire) {
+        userLevel++;
+     }
+}
+
+
+
